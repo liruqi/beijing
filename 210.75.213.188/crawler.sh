037bf40ab552ef2ad8bac67d6d53d02b71cbf1b4
liruqi <liruqi@gmail.com>
2016-03-18 01:34:20 +0800

    Fix html header

--- 210.75.213.188/crawler.sh
+++ 210.75.213.188/crawler.sh
@@ -14,11 +14,18 @@ do
 done
 fi
 
-> "audit_house_list/index.html"
+echo """<!DOCTYPE HTML>
+<html lang="zh-Hans">
+<head>
+<meta charset="UTF-8">
+</head>
+<body>
+<table>""" > "audit_house_list/index.html"
+
 for i in {1..330}
 do
     FILENAME="audit_house_list/p${i}.html"
     hxnormalize -l 240 -x $FILENAME 2>/dev/null | hxselect -s '\n' -c "table.houseList tbody" >> "audit_house_list/index.html"
 done
 
-
+echo "</table>" >> "audit_house_list/index.html"
