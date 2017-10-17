c9dc0a147ad6f3c5b19eeb1f1504a5ed5ddfb926
liruqi <liruqi@gmail.com>
2017-10-17 11:36:26 +0800

    Update script for chinapedia.org

--- cma.gov.cn/crawler.py
+++ cma.gov.cn/crawler.py
@@ -11,28 +11,41 @@ import httplib
 import os
 import errno
 
-now = int (time.time()) - 8 * 3600
-now = (now / 15 / 60 ) * 15 * 60
+
+now = int (time.time())
+now = (now / 30 / 60 ) * 30 * 60 + (15 * 60)
+utc_now_dt = datetime.datetime.fromtimestamp(now - 13 * 3600)
+print utc_now_dt
 
 host = "image.nmc.cn"
 RAWDIR = os.path.dirname(os.path.abspath(__file__)) + "/../" + host + "/product"
 
+image_dir = RAWDIR + "/%s/%s/%s/WXCL/medium" % (utc_now_dt.strftime('%Y') , utc_now_dt.strftime('%m'), utc_now_dt.strftime('%d')) 
+chinapedia_nephogram = open('/tmp/chinapedia_nephogram', 'w+')
+chinapedia_nephogram.write(image_dir + '\n')
+chinapedia_nephogram.write("%s_%s_%s" % (utc_now_dt.strftime('%Y') , utc_now_dt.strftime('%m'), utc_now_dt.strftime('%d')) + '\n')
+chinapedia_nephogram.close()
+
 conn = httplib.HTTPConnection(host, 80)
-for i in range(365):
-    ts = now - 15 * 60 * i
+for i in range(48 * 3):
+    ts = now - 30 * 60 * i
     dt = datetime.datetime.fromtimestamp(ts)
+    utc_dt = datetime.datetime.fromtimestamp(ts - 8 * 3600)
     year = dt.strftime('%Y') 
     month = dt.strftime('%m') 
     dayIM = dt.strftime('%d') 
-    tm = dt.strftime('%Y%m%d%H%M') 
+    tm = utc_dt.strftime('%Y%m%d%H%M') 
     localname=RAWDIR + "/%s/%s/%s/WXCL/medium/SEVP_NSMC_WXCL_ASC_E99_ACHN_LNO_PY_%s00000.JPG" % (year, month, dayIM, tm) 
     FILEURI="/product/%s/%s/%s/WXCL/medium/SEVP_NSMC_WXCL_ASC_E99_ACHN_LNO_PY_%s00000.JPG" % (year, month, dayIM, tm)
-
+    
+    if os.path.exists(localname):
+        print 'Exists' + i + ': ' + FILEURI
+        break
     print "get: " + FILEURI
     conn.request('GET', FILEURI)
     httpf = conn.getresponse()
     body = httpf.read()
-    if (len(body) > 1020):
+    if (len(body) > 13782):
         try:
             os.makedirs(os.path.dirname(localname))
         except OSError as exc:
@@ -44,6 +57,7 @@ for i in range(365):
         localf.write(body)
         localf.close()
         print FILEURI
-
+    else:
+        print "failed: " + FILEURI
     httpf.close()
 
--- cma.gov.cn/crawler.sh
+++ cma.gov.cn/crawler.sh
@@ -2,9 +2,14 @@
 
 TODAY=`date +"%Y-%m-%d"`
 
-python crawler.py 
+python crawler.py >> /tmp/chinapedia_nephogram.log 
 
 git add --all ../image.nmc.cn
 git commit -m "Update nephogram for $TODAY"
 git push
+
+
+cd `head -n 1 /tmp/chinapedia_nephogram`
+ffmpeg -framerate 1 -pattern_type glob -i '*.JPG' -i /root/china.mp3 -codec copy -c:v libx264 -r 30 -pix_fmt yuv420p -t 48 /srv/http/chinapedia.org/nephogram/`tail -n 1 /tmp/chinapedia_nephogram`.mp4
+cd -
 
